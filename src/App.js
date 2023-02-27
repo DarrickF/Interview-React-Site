005756d9c5f0c6ef8b1d1d3da058c339cf43751f
DarrickF <Darrick_F@live.com>
2023-02-27 00:04:33 -0600

    Before button

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Nav from './components/Nav';
+import {BrowserRouter as Router, Routes, Route,} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+<>
+<Router>
+     <Nav />
+     <Routes>
+     <Route path='/' exact />
+      </Routes>
+     </Router>
+      </>
+  );
+}
+
+export default App;
